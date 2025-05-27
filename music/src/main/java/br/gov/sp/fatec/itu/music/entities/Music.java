de6f062692990398020c10d3aa842be5eb17fcd8
LucasMorais42 <lucasmorais2214@gmail.com>
2025-05-27 14:38:18 -0300

    Criação inicial do projeto e configurações em application properties

---
+++ music/src/main/java/br/gov/sp/fatec/itu/music/entities/Music.java
@@ -0,0 +1,9 @@
+package br.gov.sp.fatec.itu.music.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
