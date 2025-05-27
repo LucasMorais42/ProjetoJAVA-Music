abf4ebed4d56dac0daa8421140388a115b9187ba
LucasMorais42 <lucasmorais2214@gmail.com>
2025-05-27 15:07:27 -0300

    Criação dos servicos de GET e POST

---
+++ music/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_MUSICS (name,singer, studio, duration, cd_solds) VALUES ('Borboletas', 'Vitor e Leo', 'Os Melhores do MPB', 120, 100000);
+INSERT INTO TBL_MUSICS (name,singer, studio, duration, cd_solds) VALUES ('Young Dumb Stupid', 'NMIXX', 'S/M Entertainment', 100, 130000);
+INSERT INTO TBL_MUSICS (name,singer, studio, duration, cd_solds) VALUES ('New Jeans', 'New Jeans', 'Ador', 30, 1300000);
